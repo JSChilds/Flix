5bcc66c3e11c6f9d19d11c0a05a66d42ac118446
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-07-18 16:11:27 +0100

    Added view files and controller methods for reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+class ReviewsController < ApplicationController
+	def index
+		@reviews = Review.all
+	end
+
+	def show
+		@review = Review.find(params[:id])
+	end
+
+	def edit
+		@review = Review.find(params[:id])
+	end
+
+	def new
+		@review = Review.new
+	end
+
+	def create
+		@review = Review.create(review_params)
+		@review.save
+		redirect_to @review
+	end
+
+	def update
+		@review = Review.find(params[:id])
+		@review.update(review_params)
+		redirect_to @review
+	end
+
+	def destroy
+		@review.destroy
+	end
+
+	private
+		def review_params
+	      params.require(:review).permit(:title, :year, :director, :description)
+	    end
+end	
