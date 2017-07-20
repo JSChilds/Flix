67ee27981b1734ee54d11e59a1006958cd89aacf
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-07-20 15:30:45 +0100

    Made helper method to define rating based on review score

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def calculate_rating reviews  
+		reviews.each do |review|
+			count = 0
+			count += review.score
+			count = count / reviews.length
+			return count
+		end
+	end
 end
