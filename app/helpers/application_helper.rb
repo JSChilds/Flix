4297cee1424d76a7e8c95c53a97712214a8aad47
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-07-20 16:54:30 +0100

    Styled show page for movies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
 			return count
 		end
 	end
+
+	def thumbs_up_down judgement
+		if judgement
+			return image_tag "thumbs_up.png", class: "thumb"
+		else
+			return image_tag "thumbs_down.png", class: "thumb"
+		end
+	end
 end
