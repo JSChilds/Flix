8b2311fecad9a81ca0c68a9741d7c1ab4dc21ac0
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-07-20 10:55:10 +0100

    Added bootsrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
